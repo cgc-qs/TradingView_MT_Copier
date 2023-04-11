6785949e1b51b98affa49208f0c7739a9fd0da82
cgc-qs <87303775+cgc-qs@users.noreply.github.com>
2023-04-11 16:58:54 +0900

    init

---
+++ app/controllers/URL.controller.js
@@ -0,0 +1,168 @@
+let OrderInfo = {
+    Type: -1,
+    Lot: 0,
+    Ticket: -1,
+    Symbol: "",
+    Position: 0
+};
+let CloseTicket = -1;
+const { Console } = require('console');
+// Create and Save a new Tutorial
+
+const fs = require('fs');
+
+exports.Check = (req, res) => {
+    res.status(200).send({ message: "!!! OK !!!" });
+    PrintLog("URL is Checked");
+};
+
+
+// Retrieve all Tutorials from the database.
+exports.Login = (req, res) => {
+    try {
+        kind = req.body.kind;
+        msg = "Slave Login is success";
+
+        if (kind == 0) {
+            OrderInfo.Type = -1;
+            OrderInfo.Lot = 0;
+            OrderInfo.Ticket = -1;
+            CloseTicket = -1;
+            OrderInfo.Symbol = "";
+            OrderInfo.Position = 0;
+            msg = "Master Login is success"
+        }
+
+        PrintLog(msg);
+        res.status(200).send({ message: msg });
+    }
+    catch (e) {
+        res.status(500).send({ message: " Error 500 : Login is Failed " + " errorMessage:" + e.message });
+        PrintLog(" Error 500 : Login is Failed " + " errorMessage:" + e.message);
+    }
+};
+
+exports.History = async (req, res) => {
+    try {
+        let msg = await ReadLog(req);
+
+        res.status(200).send({ message: msg });
+    }
+    catch (e) {
+        res.status(500).send({ message: " Error 500 : History is Failed " + " errorMessage:" + e.message });
+        PrintLog(" Error 500 : History is Failed " + " errorMessage:" + e.message);
+    }
+};
+
+exports.TVOpenSignal = (req, res) => {
+    try {
+        OrderInfo.Type = req.body.type;
+        OrderInfo.Lot = req.body.lot;
+        OrderInfo.Position = req.body.position;
+        OrderInfo.Symbol = req.body.symbol;
+        const d = new Date();
+        let time = d.getTime();
+        OrderInfo.Ticket = time;
+        msg = "New Order is stored. Type: " +
+            (OrderInfo.Type == "buy" ? "Buy," : "Sell,") +
+            " Lot: " + OrderInfo.Lot + "," +
+            " Ticket: " + OrderInfo.Ticket +
+            " Symbol: " + OrderInfo.Symbol +
+            " Position: " + OrderInfo.Position;
+        console.log(msg);
+        PrintLog(msg);
+        res.status(200).send({ message: msg });
+    }
+    catch (e) {
+        res.status(500).send({ message: " Error 500 : TVOpenSignal is Failed " + " errorMessage:" + e.message });
+        PrintLog(" Error 500 : TVOpenSignal is Failed " + " errorMessage:" + e.message);
+    }
+};
+
+exports.OpenOrder = (req, res) => {
+    try {
+        OrderInfo.Type = req.body.type;
+        OrderInfo.Lot = req.body.lot;
+        OrderInfo.Ticket = req.body.ticket;
+        msg = "New Order is stored. Type: " +
+            (OrderInfo.Type == 0 ? "Buy," : "Sell,") +
+            " Lot: " + OrderInfo.Lot + "," +
+            " Ticket: " + OrderInfo.Ticket;
+        console.log(msg);
+        PrintLog(msg);
+        res.status(200).send({ message: msg });
+    }
+    catch (e) {
+        res.status(500).send({ message: " Error 500 : OrderOpen is Failed " + " errorMessage:" + e.message });
+        PrintLog(" Error 500 : OrderOpen is Failed " + " errorMessage:" + e.message);
+    }
+};
+
+exports.CloseOrder = (req, res) => {
+    try {
+        CloseTicket = req.body.ticket;
+        msg = "Close Order is stored. Ticket: " + CloseTicket;
+        console.log(msg);
+        PrintLog(msg);
+        res.status(200).send({ message: msg });
+    }
+    catch (e) {
+        res.status(500).send({ message: " Error 500 : CloseOrder is Failed " + " errorMessage:" + e.message });
+        PrintLog(" Error 500 : CloseOrder is Failed " + " errorMessage:" + e.message);
+    }
+};
+
+exports.GetOrderInfo = (req, res) => {
+    try {
+        res.status(200).send({ message: "Information:", openInformation: OrderInfo, closeTicket: CloseTicket });
+        PrintLog("Information is success");
+    }
+    catch (e) {
+        res.status(500).send({ message: " Error 500 : GetOrderInfo is Failed " + " errorMessage:" + e.message });
+        PrintLog(" Error 500 : GetOrderInfo is Failed " + " errorMessage:" + e.message);
+    }
+};
+
+const PrintLog = (msg) => {
+    let date_ob = new Date();
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    // current year
+    let year = date_ob.getFullYear();
+    // current hours
+    let hours = date_ob.getHours();
+    // current minutes
+    let minutes = date_ob.getMinutes();
+    // current seconds
+    let seconds = date_ob.getSeconds();
+
+    // prints date & time in YYYY-MM-DD HH:MM:SS format
+    const date_time = (year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+    const fileName = year + "_" + month + "_" + date + ".txt";
+
+    fs.appendFile(fileName, date_time + " => " + msg + "\n", err => {
+        if (err) {
+            console.error(err);
+            console.log("File write is failed", fileName);
+        }
+        // file written successfully       
+    });
+}
+
+
+const ReadLog = async (req) => {
+    year = req.body.year;
+    month = req.body.month;
+    date = req.body.date;
+    const fileName = year + "_" + month + "_" + date + ".txt";
+
+    const Resultdata = await fs.promises.readFile(fileName, (err, data) => {
+        if (err) {
+            console.log(fileName + " reaing error");
+            throw err;
+        }
+    })
+    return Resultdata.toString();
+}
---
+++ app/routes/URL.routes.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+    const orderSignal = require("../controllers/URL.controller.js");
+
+    var router = require("express").Router();
+
+    // Retrieve all orderSignal
+    router.get("/", orderSignal.Check);
+
+    // Retrieve all published orderSignal
+    router.post("/Login", orderSignal.Login);
+
+    router.post("/OpenOrder", orderSignal.OpenOrder);
+
+    router.post("/CloseOrder", orderSignal.CloseOrder);
+
+    router.get("/GetOrderInfo", orderSignal.GetOrderInfo);
+
+    router.get("/History", orderSignal.History);
+
+    router.post("/TVOpenSignal", orderSignal.TVOpenSignal);
+
+
+    app.use("/RemoteCopier", router);  ///// this is base url
+};
